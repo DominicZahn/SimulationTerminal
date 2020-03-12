bf6121f9decbf2163b2cfb849d7e3f65e8e718e9
Dominic Zahn <zahndominic@web.de>
2020-03-12 13:04:38 +0100

    added methods to the header file

--- SimulationTerminal/File.h
+++ SimulationTerminal/File.h
@@ -1,5 +1,14 @@
 #pragma once
 class File
 {
+public:
+	File(string path);
+
+	string getExtension();
+	string getName();
+	string getPath();
+	// transforms a .ino to .cpp
+	// also replaces the arduino commands with the simulation commands
+	bool transformToCpp();
 };
 
